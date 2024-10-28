64916da8311c1f3c497ca3b38f32a7283bea8eb1
pitly <pitly@TEWIS-PC>
2024-10-28 16:54:59 +0200

    Added new lab #3 Matrix_mapping

---
+++ matrix_mapping.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def negative_matrix(x: np.ndarray) -> np.ndarray:
+    """
+    Returns the negation of each element in the input vector or matrix.
+
+    Args:
+        x (np.ndarray): A vector (n*1) or matrix (n*n).
+
+    Returns:
+        np.ndarray: A matrix with each element negated.
+    """
+    return -x
+
+
+def reverse_matrix(x: np.ndarray) -> np.ndarray:
+    """
+    Returns the input vector or matrix with the order of elements reversed.
+
+    Args:
+        x (np.ndarray): A vector (n*1) or matrix (n*n).
+
+    Returns:
+        np.ndarray: A matrix with the order of elements reversed.
+    """
+    return np.flip(x)
+
+def affine_transform(
+    x: np.ndarray, alpha_deg: float, scale: tuple[float, float], shear: tuple[float, float],
+    translate: tuple[float, float],
+) -> np.ndarray:
+    """Compute affine transformation
+
+    Args:
+        x (np.ndarray): vector n*1 or matrix n*n.
+        alpha_deg (float): rotation angle in deg.
+        scale (tuple[float, float]): x, y scale factor.
+        shear (tuple[float, float]): x, y shear factor.
+        translate (tuple[float, float]): x, y translation factor.
+
+    Returns:
+        np.ndarray: transformed matrix.
+    """
+    alpha_rad = np.radians(alpha_deg)
+    
+    rot_matrix = np.array([
+        [np.cos(alpha_rad), -np.sin(alpha_rad)],
+        [np.sin(alpha_rad),  np.cos(alpha_rad)]
+    ])
+    
+    scal_matrix = np.array([
+        [scale[0], 0],
+        [0, scale[1]]
+    ])
+
+    shear_matrix = np.array([
+        [1, shear[0]],
+        [shear[1], 1]
+    ])
+    
+    affine_matrix = rot_matrix @ scal_matrix @ shear_matrix
+    result = (affine_matrix @ x.T).T + np.array(translate)
+    return result
